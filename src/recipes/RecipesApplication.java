d006c343ac0ce59ddae90c7a4efb0bbaf580fb69
yassir <yassirdriuch@gmail.com>
2023-12-08 22:17:35 +0100

    little update

--- src/recipes/RecipesApplication.java
+++ src/recipes/RecipesApplication.java
@@ -9,11 +9,3 @@ public class RecipesApplication {
         SpringApplication.run(RecipesApplication.class, args);
     }
 }
-
-echo "# RecipeManagementSystem" >> README.md
-        git init
-        git add *
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/YassirDriuch/RecipeManagementSystem.git
-        git push -u origin main
